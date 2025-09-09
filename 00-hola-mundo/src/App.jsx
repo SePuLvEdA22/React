4dd6be94c7bfb8ad47132d8d4424c3cffac0b2a9
Helger <helgerjo123@gmail.com>
2025-09-09 08:43:55 -0500

    adding styles and improving the UI

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -1,19 +1,46 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+  {
+    userName: 'helger',
+    name: 'Helger Jose Santiago',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Hernandez',
+    isFollowing: false
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Durán',
+    isFollowing: false
+  },
+  {
+    userName: 'gndx',
+    name: 'Gonzalo Nuñez de Caceres',
+    isFollowing: true
+  }
+]
+
 export default function App () {
 
   return (
     <section className='App'>
-      <TwitterFollowCard 
-        userName='helgersantiago'>
-          Helger Jose Santiago
-      </TwitterFollowCard>
-      <TwitterFollowCard 
-        userName='pheralb' >
-          Pablo Hernandez
-      </TwitterFollowCard>
-
+      {
+        users.map((user) => {
+          const { userName, name, isFollowing } = user
+          return (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              name={name}
+              isFollowing={isFollowing}
+            > {name} </TwitterFollowCard>
+          ) 
+        })
+      }
     </section>
   )
 }
